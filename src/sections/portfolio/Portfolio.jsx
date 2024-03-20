8ebdb3baec6e4921a3d022fefdcdd53aa6823b66
steph-ayo <popoolastephen19@gmail.com>
2024-03-20 23:48:16 +0100

    portfolio section update

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,6 +1,14 @@
 import "./portfolio.css";
+import Projects from "./Projects";
+import ProjectsCategories from "./ProjectsCategories";
+import data from "./data";
+import { useState } from "react";
 
 const Portfolio = () => {
+  const [projects, setProjects] = useState(data);
+
+  const categories = data.map((item) => item.category);
+
   return (
     <section id="portfolio">
       <h2>Recent Projects</h2>
@@ -8,6 +16,10 @@ const Portfolio = () => {
         Check some of the projects I recently worked on for my clients. Use the
         butoons to toggle the different categories.
       </p>
+      <div className="container portfolio__container">
+        <ProjectsCategories />
+        <Projects projects={projects} />
+      </div>
     </section>
   );
 };
---
+++ src/sections/portfolio/ProjectsCategories.jsx
@@ -0,0 +1,5 @@
+const ProjectsCategories = () => {
+  return <div>ProjectsCategories</div>;
+};
+
+export default ProjectsCategories;
