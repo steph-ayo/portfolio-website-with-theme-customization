5e8af0dc272459f820d5d771765d1bbd364f0b3e
steph-ayo <popoolastephen19@gmail.com>
2024-03-23 22:16:04 +0100

    contact section update

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,9 +1,23 @@
+import contacts from "./data";
 import "./contact.css";
 
 const Contact = () => {
   return (
     <section id="contact">
-      <h2>CONTACT</h2>
+      <h2>Get In Touch</h2>
+      <p>Shoot me a message via any of the links below!</p>
+      <div className="container contact__container">
+        {contacts.map((contact) => (
+          <a
+            key={contact.id}
+            href={contact.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {contact.icon}
+          </a>
+        ))}
+      </div>
     </section>
   );
 };
