81d6c0415f1e1b121fee596ee49b7a59cb5938b4
steph-ayo <popoolastephen19@gmail.com>
2024-03-27 15:02:48 +0100

    Final completion

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,21 +1,28 @@
+import { useEffect } from "react";
 import HeaderImage from "../../assets/header.jpg";
 import data from "./data";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./header.css";
 
 const Header = () => {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
   return (
     <header id="header">
       <div className="container header__container">
-        <div className="header__profile">
+        <div className="header__profile" data-aos="fade-in">
           <img src={HeaderImage} alt="Header Portait" />
         </div>
-        <h3>Popoola Stephen</h3>
-        <p>
+        <h3 data-aos="fade-up">Hajia Bintu</h3>
+        <p data-aos="fade-up">
           You are a click away from building your dream website or web app. Send
           me the details of your project for a modern, mobile responsive, highly
           performant website today!
         </p>
-        <div className="header__cta">
+        <div className="header__cta" data-aos="fade-up">
           <a href="#contact" className="btn primary">
             Let's Talk
           </a>
